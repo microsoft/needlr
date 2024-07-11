3332909defb1fb1cd3ec427b45c0a5543491bc9d
Bret Myers <bretamyers@gmail.com>
2024-07-11 08:54:05 -0400

    Long running support
    Added long running operations support.

--- needlr/auth/scopes.py
+++ needlr/auth/scopes.py
@@ -6,6 +6,8 @@ CAPACITY_RW_ALL = "https://api.fabric.microsoft.com/Capacity.ReadWrite.All"
 
 AZ_STORAGE = "https://storage.azure.com/user_impersonation"
 
+AZ_STORAGE_DEFAULT = "https://storage.azure.com/.default"
+
 AZ_DATABASE = "https://database.windows.net//.default"
 
 AZ_MANAGEMENT = "https://management.core.windows.net/.default"
