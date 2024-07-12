fcc0f61d8d3917c5ab0f0df9721a6b0f1ecdd476
Tonio Lora <tonio.lora@microsoft.com>
2024-07-12 13:48:28 -0500

    Semantic Model tests updated

--- tests/powerbi/test_semanticmodel.py
+++ tests/powerbi/test_semanticmodel.py
@@ -25,7 +25,6 @@ class TestSemanticModelLifeCycle:
         definition = fc.semanticmodel.get_definition(workspace_id=workspace_test.id, semanticmodel_id=test_semanticmodel.id)
         assert definition['parts'] is not None
 
-    @pytest.mark.skip(reason="Waiting for create to work")
     def test_semanticmodel_delete(self, fc: FabricClient, workspace_test: Workspace, test_semanticmodel:SemanticModel):
         resp = fc.semanticmodel.delete(workspace_id=workspace_test.id, semanticmodel_id=test_semanticmodel.id)
         assert resp.is_successful is True
