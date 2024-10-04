dafec2f89f41e2a3c25fb73930e0c0b0788fb027
Timothy J Brown <nextdynamic@gmail.com>
2024-10-04 15:33:31 -0400

    Base commit to Git working

---
+++ needlr/core/git/git.py
@@ -0,0 +1,118 @@
+"""Module providing Core Capacity functions."""
+
+from collections.abc import Iterator
+from needlr import _http
+from needlr.auth.auth import _FabricAuthentication
+from needlr.models.git import GitStatusResponse, CommitToGitRequest, ItemIdentifier
+
+
+class _GitClient():
+    """
+
+    [Reference](https://learn.microsoft.com/en-us/rest/api/fabric/core/git)
+
+    Methods:
+
+    get_status: Return the Git Status of items in the workspace that can be committed to Git
+
+    """
+    def __init__(self, auth:_FabricAuthentication, base_url):
+        """
+        Initializes a GitClient object.
+
+        Args:
+            auth (_FabricAuthentication): An instance of the _FabricAuthentication class.
+            base_url (str): The base URL for the role.
+
+        Returns:
+            None
+        """
+        self._auth = auth
+        self._base_url = base_url
+
+    def commit_to_git(self, workspace_id:str, mode:str, comment:str, items: list[ItemIdentifier]):
+        """
+        Commits the changes made in the workspace to the connected remote branch.
+        This API supports long running operations (LRO).
+        You can choose to commit all changes or only specific changed items. To sync the workspace for the first time, use this API after the Connect and Initialize Connection APIs.
+
+        Parameters:
+        - workspace_id (str): The ID of the workspace where the item will be created.            
+        - mode:  Modes for the Commit operation.  Refer to (https://learn.microsoft.com/en-us/rest/api/fabric/core/git/commit-to-git?tabs=HTTP#commitmode)
+        - comment:  The comment that will be assigned for the commmit.
+
+        Returns:
+            [Iterator]GitStatusResponse An iterator that yields Workspace objects representing each workspace.
+
+        Reference:
+        - [Git - Commit to Git](https://learn.microsoft.com/en-us/rest/api/fabric/core/git/commit-to-git?tabs=HTTP)
+        """
+
+        # get the workspacehead string which is required for the body to the commit
+        #wsh = self.__getWorkspaceHead__(self, workspace_id)
+        gitStatus = self.get_status( workspace_id )
+        wsh = gitStatus.workspaceHead
+
+        #TODO Maybe throw an exception is wsh has a null value
+
+        #TODO Throw an exception for a bad mode?
+        #if mode != CommitMode.All or CommitMode.Selective:
+
+        body = {
+                "mode": mode,
+                "workspaceHead": wsh,
+                "comment": comment
+        }
+
+        if items:
+            body["items"] = items
+
+        resp = _http._post_http_long_running(
+            url = f"{self._base_url}workspaces/{workspace_id}/git/commitToGit",
+            auth=self._auth,
+            item=CommitToGitRequest(**body)
+        ) 
+
+    def __getWorkspaceHead__(self, workspace_id:str) -> str:
+        """
+        Get the workspacehead string from the response object.  This is needed for other methods.
+
+        Args:
+            - workspace_id (str): The ID of the workspace where the item will be created.
+
+        Returns:
+            The workspacehead as a string
+        """
+        
+        gitStatus = self.get_status(self, workspace_id)
+
+        return gitStatus.workspaceHead
+
+    def get_status(self, workspace_id:str) -> GitStatusResponse:
+        """
+        Returns the Git status of items in the workspace, that can be committed to Git.
+        This API supports long running operations (LRO).
+        The status indicates changes to the item(s) since the last workspace and remote branch sync. If both locations were modified, the API flags a conflict.
+
+        Parameters:
+        - base_url (str): The base URL of the fabric API.
+        - workspace_id (str): The ID of the workspace where the item will be created.            
+        - auth (_FabricAuthentication): The authentication object for making API requests.
+
+        Returns:
+            [Iterator]GitStatusResponse An iterator that yields Workspace objects representing each workspace.
+
+        Reference:
+        - [Git - Get Status](https://learn.microsoft.com/en-us/rest/api/fabric/core/git/get-status?tabs=HTTP)
+        """
+        #print( "Getting status for url: "+ self._base_url+f"workspaces/{workspace_id}/git/status")
+
+        resp = _http._get_http(
+            url = self._base_url+f"workspaces/{workspace_id}/git/status",
+            auth= self._auth,
+            items_extract=lambda x:x["changes"]
+
+        )
+
+        return GitStatusResponse(**resp.body)
+   
