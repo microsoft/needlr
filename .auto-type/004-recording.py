3ae790cf38344df43275b1a898b826ad7d35a973
Tonio Lora <tonio.lora@microsoft.com>
2024-10-07 09:15:25 -0500

    Removing recording files

--- .auto-type/004-recording.py
+++
@@ -1,5 +0,0 @@
-file: samples/ds.py
-line: 23
----
-        fc.workspace.capacity_assign(ws.id, capacity_id=fc.workspace.get('LargeDataScienceWS').capacityId)
-        fc.workspace.role.assign(ws.id, principal=row['DS_GUID'], role='Contributor')
