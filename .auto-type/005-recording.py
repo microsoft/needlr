584ba65d6e90e537ce80730065f056bb1dd7bd9f
Tonio Lora <tonio.lora@microsoft.com>
2024-10-07 13:58:54 -0500

    Added init files for packaging to PyPi (#12)
    * Added new Susbs ID
    
    * Added .env to gitignore
    
    * Change on capacity id
    
    * Removing recording files
    
    * Init files added so they can be part of the distribution

--- .auto-type/005-recording.py
+++
@@ -1,4 +0,0 @@
-file: samples/ds.py
-line: 25
----
-        fc.workspace.git.connect(ws.id, organization_Name='Data Science Repo', project='DS Project', gitProviderType='AzureDevOps', repository='DS Repo', branch='main')
