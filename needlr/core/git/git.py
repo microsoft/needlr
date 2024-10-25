3a3d12941bbb8b1671dcb0845831b3d4b82e35ef
Timothy J Brown <nextdynamic@gmail.com>
2024-10-25 11:43:23 -0400

    Git tests

--- needlr/core/git/git.py
+++ needlr/core/git/git.py
@@ -2,8 +2,9 @@
 
 from collections.abc import Iterator
 from needlr import _http
+from requests import Response
 from needlr.auth.auth import _FabricAuthentication
-from needlr._http import FabricResponse
+from needlr._http import FabricResponse, FabricException
 from needlr.models.git import (
     GitStatusResponse,
     CommitToGitRequest,
@@ -71,10 +72,10 @@ class _GitClient:
         gitStatus = self.get_status(workspace_id)
         wsh = gitStatus.workspaceHead
 
-        # TODO Maybe throw an exception is wsh has a null value
-
         # TODO Throw an exception for a bad mode?
         # if mode != CommitMode.All or CommitMode.Selective:
+
+        #if gitStatus.changes.count > 0:
 
         body = {"mode": mode, "workspaceHead": wsh, "comment": comment}
 
@@ -83,12 +84,17 @@ class _GitClient:
             if items:
                 body["items"] = items
 
+        # 10.21.24 Confered with Bret.  If an attempt to commit fails due to a lack of items needing to be committed,
+        # a 400 error is raised with the message -- "errorCode":"NoChangesToCommit","message":"There are no changes to commit."
+
         resp = _http._post_http_long_running(
             url=f"{self._base_url}workspaces/{workspace_id}/git/commitToGit",
             auth=self._auth,
             item=CommitToGitRequest(**body),
         )
         return resp
+            
+
 
     def connect(self, workspace_id: uuid.UUID, gpd: GitProviderDetails) -> FabricResponse:
         """
@@ -276,7 +282,7 @@ class _GitClient:
     
 
     def update_from_git(
-        self, workspace_id: str, conflictResolution: WorkspaceConflictResolution, options: UpdateOptions
+        self, workspace_id: uuid.UUID, conflictResolution: WorkspaceConflictResolution, options: UpdateOptions
     ) -> FabricResponse:
         """
         Updates the workspace with commits pushed to the connected branch.
@@ -298,7 +304,11 @@ class _GitClient:
         wsh = gitStatus.workspaceHead
         rch = gitStatus.remoteCommitHash
 
-        body = {"conflictResolution": conflictResolution, "options": options, "remoteCommitHash": rch, "workspaceHead": wsh}   
+        body = {"conflictResolution": conflictResolution, "options": options, "remoteCommitHash": rch }
+
+        if wsh != 'None': # only add the items if the mode if not Null
+            body["workspaceHead"] = wsh
+            
 
         resp = _http._post_http_long_running(
             url=f"{self._base_url}workspaces/{workspace_id}/git/updateFromGit",
