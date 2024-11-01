5262a64cea36eba67a379cb162b9a6b18bf11224
Tonio Lora <tonio.lora@microsoft.com>
2024-11-01 00:01:00 -0500

    Fixed data pipeline update ad related tests

--- tests/datafactory/test_datapipeline.py
+++ tests/datafactory/test_datapipeline.py
@@ -1,39 +1,41 @@
+import pytest
+
 from needlr import FabricClient
 from needlr.models.workspace import Workspace
 from needlr.models.report import Report
+from needlr.models.datapipeline import Datapipeline
 
 class TestDatapipelineLifeCycle:
 
-    def test_datapipeline_ls(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report):
-        rpts = fc.datapipeline.ls(workspace_id=workspace_test.id)
-        for r in list(rpts):
-            if r.id == test_datapipeline.id:
+    def test_datapipeline_ls(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Datapipeline):
+        dp = fc.datapipeline.ls(workspace_id=workspace_test.id)
+        for p in list(dp):
+            if p.id == test_datapipeline.id:
                 assert True
                 return
+    @pytest.mark.order(after="test_datapipeline_ls")
+    def test_datapipeline_get(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Datapipeline):
+        dp = fc.datapipeline.get(workspace_id=workspace_test.id, datapipeline_id=test_datapipeline.id)
+        assert dp is not None
 
-    def test_datapipeline_get(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report):
-        sm = fc.datapipeline.get(workspace_id=workspace_test.id, report_id=test_datapipeline.id)
-        assert sm is not None
-
-    def test_datapipeline_update_definition(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report, rpt_definition:dict):
-        res = fc.datapipeline.update_definition(workspace_id=workspace_test.id, report_id=test_datapipeline.id, definition=rpt_definition)
+    @pytest.mark.order(after="test_datapipeline_get")
+    def test_datapipeline_update(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Datapipeline):
+        res = fc.datapipeline.update(workspace_id=workspace_test.id, datapipeline_id=test_datapipeline.id,display_name="updated_pipeline_name", description='updated description')
         assert res.is_successful is True
 
-    def test_datapipeline_get_definition(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report):
-        definition = fc.datapipeline.get_definition(workspace_id=workspace_test.id, report_id=test_datapipeline.id)
-        assert definition['parts'] is not None
-
-    def test_datapipeline_clone(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report):
+    @pytest.mark.order(after="test_datapipeline_update")
+    def test_datapipeline_clone(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Datapipeline):
         cloned = fc.datapipeline.clone(source_workspace_id=workspace_test.id, 
-                                 report_id=test_datapipeline.id, 
+                                 datapipeline_id=test_datapipeline.id, 
                                  clone_name=f'{test_datapipeline.name}_cloned',
                                  target_workspace_id=workspace_test.id)
         assert cloned is not None
         if cloned is not None:
-            fc.datapipeline.delete(workspace_id=workspace_test.id, report_id=cloned.id)    
+            fc.datapipeline.delete(workspace_id=workspace_test.id, datapipeline_id=cloned.id)    
 
-    def test_datapipeline_delete(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Report):
-        resp = fc.datapipeline.delete(workspace_id=workspace_test.id, report_id=test_datapipeline.id)
+    @pytest.mark.order(after="test_datapipeline_clone")
+    def test_datapipeline_delete(self, fc: FabricClient, workspace_test: Workspace, test_datapipeline:Datapipeline):
+        resp = fc.datapipeline.delete(workspace_id=workspace_test.id, datapipeline_id=test_datapipeline.id)
         assert resp.is_successful is True
         
         
