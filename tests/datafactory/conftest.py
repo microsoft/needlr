3da2134c2a5a71acfe61f2883e81ae678bbdc1d3
Tonio Lora <tonio.lora@microsoft.com>
2024-11-04 20:20:40 -0600

    Data Pipelines run methods added

--- tests/datafactory/conftest.py
+++ tests/datafactory/conftest.py
@@ -1,10 +1,18 @@
 from typing import Generator
 import pytest
+import pickle
 from needlr import auth, FabricClient
 from needlr.models.datapipeline import Datapipeline
 from needlr.models.workspace import Workspace
 
 @pytest.fixture(scope='session')
-def test_datapipeline(fc: FabricClient, workspace_test:Workspace, testParameters) -> Generator[Datapipeline, None, None]:
-    dp = fc.datapipeline.create(display_name=testParameters['datapipeline_name'], workspace_id=workspace_test.id, description=testParameters['datapipeline_description'])
+def datapipeline_definition():
+    with open('tests/datafactory/datapipeline_definition.pkl', 'rb') as sample_datapipeline_definition:
+        definition = pickle.load(sample_datapipeline_definition)
+        yield definition
+
+@pytest.fixture(scope='session')
+def test_datapipeline(fc: FabricClient, workspace_test:Workspace, datapipeline_definition:dict, testParameters: dict[str, str]) -> Generator[Datapipeline, None, None]:
+    dp = fc.datapipeline.create(display_name=testParameters['datapipeline_name'], workspace_id=workspace_test.id,
+                                definition=datapipeline_definition, description=testParameters['datapipeline_description'])
     yield dp
