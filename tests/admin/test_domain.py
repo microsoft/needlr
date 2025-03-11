e942431450083e52157a1a67891a43e921545eae
Timothy J Brown <nextdynamic@gmail.com>
2025-03-11 15:49:38 -0400

    Started the domain tests

--- tests/admin/test_domain.py
+++ tests/admin/test_domain.py
@@ -1,8 +1,8 @@
 import pytest
-
+import random as r
 from typing import Generator
 from needlr import auth, FabricClient
-from needlr.models.domain import Domain as ds
+from needlr.models.domain import Domain
 
 
 
@@ -11,6 +11,24 @@ class TestDomain:
     def test_domain_ls(self, fc: FabricClient):
         resp = fc.domain.ls()
         assert len(list(resp)) > 0
+
+    @pytest.mark.order(after="test_domain_ls")
+    def test_domain_get(self, fc: FabricClient, domain_test:Domain, testParameters: dict[str, str]):
+        testOneDomain_id = domain_test.id
+        testDomain = fc.domain.get( testOneDomain_id)
+        assert type(testDomain) is Domain
+
+
+    @pytest.mark.order(after="test_domain_get")
+    def test_domain_update(self, fc: FabricClient, domain_test: Domain, testParameters: dict[str, str]):
+        domain_id = domain_test.id
+        dom_display_name='new'+testParameters['domain_displayName']
+        dom_description='New'+testParameters['domain_description']
+
+        fc.domain.update(domain_id, dom_display_name, dom_description)
+        testDomain = fc.domain.get( domain_id )
+        assert testDomain.displayName == dom_display_name
+        assert testDomain.description == dom_description
 
    
         
